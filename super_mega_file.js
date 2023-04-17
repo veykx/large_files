8bc4ab0b96e696b49e79b96ca50d567dab19e0e3
veykx <dpla@foreworth.com>
2023-04-17 10:35:48 +0200

    Deleting rows

--- super_mega_file.js
+++ super_mega_file.js
@@ -2,12 +2,6 @@ let xMax = canvas.height;
 let slope = 1.2;
 let intercept = 70;
 
-// Plot Scatter
-ctx.moveTo(0, intercept);
-ctx.lineTo(xMax, f(xMax));
-ctx.strokeStyle = "black";
-ctx.stroke();
-
 // Line Function
 function f(x) {
   return x * slope + intercept;
