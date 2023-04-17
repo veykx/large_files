515131ea0acec79e6197ce35f4631d62222ceca0
veykx <dpla@foreworth.com>
2023-04-17 12:04:54 +0200

    Second change

--- super_mega_file.js
+++ super_mega_file.js
@@ -14,3 +14,16 @@ ctx.moveTo(0, intercept);
 ctx.lineTo(xMax, f(xMax));
 ctx.strokeStyle = "black";
 ctx.stroke();
+
+// Line Function
+function f(x) {
+  return x * slope + intercept;
+}let xMax = canvas.height;
+let slope = 1.2;
+let intercept = 70;
+
+// Plot Scatter
+ctx.moveTo(0, intercept);
+ctx.lineTo(xMax, f(xMax));
+ctx.strokeStyle = "black";
+ctx.stroke();
