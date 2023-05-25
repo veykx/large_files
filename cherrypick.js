26059d3a404ae8777e414d96cfa1bb42a8a9fa7d
Jon <jmunoa91@gmail.com>
2023-05-25 12:47:48 +0200

    Create cherrypick.js

---
+++ cherrypick.js
@@ -0,0 +1,37 @@
+//Get the file input element by its id 
+var file = document.getElementById('file');
+
+//Add a change listener to it.
+file.addEventListener('change', function() {
+  var size = file.files[0].size/1024; //To convert in KB
+  
+  //Print the final size in KB
+  console.log(size + 'KB');
+});//Get the file input element by its id 
+var file = document.getElementById('file');
+
+//Add a change listener to it.
+file.addEventListener('change', function() {
+  var size = file.files[0].size/1024; //To convert in KB
+  
+  //Print the final size in KB
+  console.log(size + 'KB');
+});//Get the file input element by its id 
+var file = document.getElementById('file');
+
+//Add a change listener to it.
+file.addEventListener('change', function() {
+  var size = file.files[0].size/1024; //To convert in KB
+  
+  //Print the final size in KB
+  console.log(size + 'KB');
+});//Get the file input element by its id 
+var file = document.getElementById('file');
+
+//Add a change listener to it.
+file.addEventListener('change', function() {
+  var size = file.files[0].size/1024; //To convert in KB
+  
+  //Print the final size in KB
+  console.log(size + 'KB');
+});
